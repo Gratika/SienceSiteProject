54733a94023a06f24bb70ad5d64f1531f537716b
Світлана Поліщук <Gratika1@gmail.com>
2023-09-28 00:17:47 +0300

    змінила рядок підключення, додала клієнта і його контейнер

--- apiServer/Models/ArhivistDbContext.cs
+++ apiServer/Models/ArhivistDbContext.cs
@@ -8,6 +8,11 @@ namespace apiServer.Models
         public ArhivistDbContext(DbContextOptions<ArhivistDbContext> options) : base(options)
         {
         }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            string mySqlConnectionString = $"server=mysql_db;port=3306;database={Environment.GetEnvironmentVariable("MYSQL_DATABASE")};user={Environment.GetEnvironmentVariable("MYSQL_USER")};password={Environment.GetEnvironmentVariable("MYSQL_PASSWORD")};";
+            optionsBuilder.UseMySQL(mySqlConnectionString);
+        }
 
     }
 }
--- apiServer/Program.cs
+++ apiServer/Program.cs
@@ -4,17 +4,16 @@ using System.Configuration;
 
 var builder = WebApplication.CreateBuilder(args);
 
+
+
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-string connectionString = "server=mysql_db;port=3306;database=${MYSQL_DATABASE};user=${MYSQL_USER};password=${MYSQL_PASSWORD};";
 
-
-builder.Services.AddDbContext<ArhivistDbContext>(options =>
-    options.UseMySQL(connectionString));
+builder.Services.AddDbContext<ArhivistDbContext>( o => o.UseMySQL(builder.Configuration.GetConnectionString("DefaultConnection")));
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
